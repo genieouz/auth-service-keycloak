1627308191fd45c99c4e504dbdf0d1f089e3ed79
genieouz <33516121+genieouz@users.noreply.github.com>
2025-06-19 12:12:43 +0000

    Corriger l'erreur de syntaxe dans KeycloakService

--- src/keycloak/keycloak.service.ts
+++ src/keycloak/keycloak.service.ts
@@ -235,6 +235,7 @@ export class KeycloakService {
       this.logger.error('Erreur lors de la création de l\'utilisateur', error.response?.data);
       throw new BadRequestException('Impossible de créer l\'utilisateur dans Keycloak');
     }
+    }
   }
 
   /**
